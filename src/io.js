75097de8c65b6d619cac431156484981b65c8871
Daniel Llatas Spiers <danllatas@gmail.com>
2018-11-25 23:45:06 +0100

    Calculate option values before capture

--- src/io.js
+++ src/io.js
@@ -1,20 +1,33 @@
-// Capture input from shell
-exports.capture = (input, elementsToIgnore = 2) => {
-  const options = {
+const options = {
     dir: '',
     format: '',
-  };
+};
+
+const optionsKey = Object.keys(options);
+
+// Capture input from shell
+const capture = (input) => {
+
+  const captured = {}
+  const elementsToIgnore = 2
+  const elementsInOption = 2
 
   for (let i = elementsToIgnore; i < input.length; i += 1) {
     const option = input[i].split('=');
-    if (option.length < elementsToIgnore) {
+    if (option.length !== elementsInOption) {
       continue;
     }
 
-    if (Object.keys(options).includes(option[0])) {
-      options[option[0]] = option[1];
+    if (optionsKey.includes(option[0])) {
+      captured[option[0]] = option[1];
     }
   }
 
-  return options;
+  return captured;
 };
+
+module.exports = {
+  capture,
+  options,
+  optionsKey
+}
