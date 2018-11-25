bca10db73661148154838c5b761be55312265d6f
Daniel Llatas Spiers <danllatas@gmail.com>
2018-11-25 21:40:47 +0100

    Move functionality for dump and file writing to cli option only

--- teseo-cli.js
+++ teseo-cli.js
@@ -1,9 +1,13 @@
 const teseo = require('./index');
 const io = require('./src/io');
+const conversion = require('./src/conversion');
+const file = require('./src/file');
 
 async function teseoCli(input) {
   const options = io.capture(input);
-  await teseo(options);
+  const result = await teseo(options);
+  const dumped = await conversion.execute('dump', options.format, result.order);
+  await file.write(dumped);
 }
 
 teseoCli(process.argv);
