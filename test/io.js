ffcb7793c2953f854843483949e316be7f03d500
Daniel Llatas Spiers <danllatas@gmail.com>
2018-11-26 12:57:21 +0100

    Update io tests

--- test/io.js
+++ test/io.js
@@ -5,14 +5,16 @@ const io = require('../src/io');
 describe('io functions test suite', () => {
   const input = ['node', 'teseo', 'dir=./test', 'format=yaml'];
 
-  it('capture should return an object with the same number of props as optionsKey', () => {
+  it('capture should return an object with at least the same number of mandatory props as optionsKey', () => {
     const captured = io.capture(input);
-    assert.equal(Object.keys(captured).length, io.optionsKey.length);
+    // assert.equal(Object.keys(captured).length, io.optionsKey.length);
+    assert.equal(Object.keys(captured).length, 2);
   });
 
-  it('capture should return an object with the same props as optionsKey', () => {
+  it('capture should return an object with at least the same mandatory props as optionsKey', () => {
     const captured = io.capture(input);
-    assert.deepEqual(Object.keys(captured), io.optionsKey);
+    // assert.deepEqual(Object.keys(captured), io.optionsKey);
+    assert.deepEqual(Object.keys(captured), ['dir', 'format']);
   });
 
   it('capture should not split input option if it does not contain symbol =', () => {
