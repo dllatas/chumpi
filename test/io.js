833e30edeb91ed8fa6940c7e56eee32735a63106
Daniel Llatas Spiers <danllatas@gmail.com>
2018-11-26 11:18:44 +0100

    Add tests for the io module

--- test/io.js
+++ test/io.js
@@ -9,4 +9,21 @@ describe('io functions test suite', () => {
     const captured = io.capture(input);
     assert.equal(Object.keys(captured).length, io.optionsKey.length);
   });
+
+  it('capture should return an object with the same props as optionsKey', () => {
+    const captured = io.capture(input);
+    assert.deepEqual(Object.keys(captured), io.optionsKey);
+  });
+
+  it('capture should not split input option if it does not contain symbol =', () => {
+    const wrongInput = ['node', 'teseo', 'dir=./test', 'format:yaml'];
+    const captured = io.capture(wrongInput);
+    assert.notStrictEqual(Object.keys(captured), io.optionsKey);
+  });
+
+  it('capture should not add to output a prop not defined in options', () => {
+    const wrongInput = ['node', 'teseo', 'dest=./test', 'format=yaml'];
+    const captured = io.capture(wrongInput);
+    assert.notStrictEqual(Object.keys(captured), io.optionsKey);
+  });
 });
