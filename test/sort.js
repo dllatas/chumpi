1f544442c9ee7ea963a0e37790ecd47ac71f6a8a
Daniel Llatas Spiers <danllatas@gmail.com>
2018-11-26 12:12:48 +0100

    Add tests for sort module

---
+++ test/sort.js
@@ -0,0 +1,19 @@
+const { describe, it } = require('mocha');
+const { assert } = require('chai');
+const sort = require('../src/sort');
+
+describe('sort module test suite', () => {
+  it('isParentIncluded returns false', () => {
+    const sorted = ['master', 'detail'];
+    const parent = ['neo'];
+    const actual = sort.isParentIncluded(sorted, parent);
+    assert.isFalse(actual);
+  });
+
+  it('sort discards a child without parent in sort', () => {
+    const tables = [{ name: 'master' }, { name: 'neo', parent: ['detail'] }, { name: 'detail', parent: ['master'] }];
+    const expected = { order: ['master', 'detail', 'neo'] };
+    const actual = sort.execute(tables);
+    assert.deepEqual(actual, expected);
+  });
+});
