65725d7e9fda0795041ee67de35345b0af16b523
Daniel Llatas Spiers <danllatas@gmail.com>
2018-11-29 21:42:14 +0100

    Call validation enforce in teseo

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+const validation = require('./src/validation');
 const file = require('./src/file');
 const conversion = require('./src/conversion');
 const sort = require('./src/sort');
 
 async function teseo(options) {
-  // TODO: validate that options have all the props needed in the right type
+  validation.enforce(options);
   const files = await file.read(options.dir);
   const tables = await conversion.execute('parse', options.format, files);
   // TODO: validate that props name always exist
