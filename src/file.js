54c28e7103a2ad71826403f8690f96a350af45e7
Daniel Llatas Spiers <danllatas@gmail.com>
2018-11-30 11:58:22 +0100

    Add createDir function and rewrite write function

--- src/file.js
+++ src/file.js
@@ -5,6 +5,7 @@ const util = require('util');
 const readDirPromise = util.promisify(fs.readdir);
 const readFilePromise = util.promisify(fs.readFile);
 const writeFilePromise = util.promisify(fs.writeFile);
+const mkDirPromise = util.promisify(fs.mkdir);
 
 // Read filenames from the dir option
 const list = async function list(dir) {
@@ -21,21 +22,37 @@ const read = async function read(dir) {
 
 const makeFilename = (dest, format) => {
   const timestamp = Date.now().toString();
-  const filename = `${path.dirname(__dirname)}/${dest}/${timestamp}.${format}`;
+  const filename = `${dest}/${timestamp}.${format}`;
   return filename;
 };
 
+const createDir = async (dirname) => {
+  const dirCreated = await mkDirPromise(dirname, { recursive: true });
+  if (!dirCreated) {
+    return;
+  }
+  if (dirCreated.code === 'EEXIST') {
+    return;
+  }
+  throw dirCreated;
+};
+
 const makeDirname = (dest) => {
+  if (path.isAbsolute(dest)) {
+    return dest;
+  }
   const dirname = `${path.dirname(__dirname)}/${dest}`;
   return dirname;
 };
 // Write into file
-const write = async function write(content, format, options = {}) {
-  const { dest = 'output' } = options;
+const write = async function write(content, options) {
+  const { format, dest = 'output' } = options;
   if (Array.isArray(content)) {
     content = content[0];
   }
-  const filename = makeFilename(dest, format);
+  const dirname = makeDirname(dest);
+  await createDir(dirname);
+  const filename = makeFilename(dirname, format);
   await writeFilePromise(filename, content);
   return filename;
 };
