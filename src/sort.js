003163064e957d9b9d3dfc301021a9c68ed28fc1
Daniel Llatas Spiers <danllatas@gmail.com>
2018-12-02 14:11:26 +0100

    Add analyzer and enforce functions to clean up and validate input

--- src/sort.js
+++ src/sort.js
@@ -1,3 +1,16 @@
+const validation = require('./validation');
+
+const schema = {
+  _name: {
+    mandatory: true,
+    type: 'string',
+  },
+  _master: {
+    mandatory: false,
+    type: 'array',
+  },
+};
+
 const isParentIncluded = (sorted, master) => {
   for (const p of master) {
     if (!sorted.includes(p)) {
@@ -7,11 +20,40 @@ const isParentIncluded = (sorted, master) => {
   return true;
 };
 
+const retrieve = (source, props) => {
+  const index = props[0];
+  const _source = source[index];
+  props.shift();
+  if (props.length === 0) {
+    return _source;
+  }
+  return retrieve(_source, props);
+};
+
+const get = (source, props, options = {}) => {
+  const { delimiter = '.', mandatory = false } = options;
+  const _props = props.split(delimiter);
+  const retrieved = retrieve(source, _props);
+  if (mandatory && !retrieved) {
+    throw new Error(`The id ${props}did not found a value.`);
+  }
+  return retrieved;
+};
+
+const analyzer = (tables, name = 'name', master = 'master') => {
+  const analyzed = tables.map(t => ({
+    _name: get(t, name, { mandatory: true }),
+    _master: get(t, master),
+  }));
+  return analyzed;
+};
+
 const execute = (tables, master = 'master', name = 'name') => {
-  // Funny part, solving the dependency
-  // Dont assume a starting point: let's check for all the tables without a master
-  const sorted = tables.filter(t => !t[master]).map(t => t[name]);
-  let unsorted = tables.filter(t => t[master]);
+  const analyzed = analyzer(tables, name, master);
+  validation.enforce(analyzed, schema);
+
+  const sorted = analyzed.filter(t => !t._master).map(t => t._name);
+  let unsorted = analyzed.filter(t => t._master);
 
   if (sorted.length === tables.length) {
     return { order: sorted };
@@ -22,11 +64,11 @@ const execute = (tables, master = 'master', name = 'name') => {
   // sortedTables has length == 0
   while (unsorted.length > 0) {
     for (const us of unsorted) {
-      if (isParentIncluded(sorted, us[master])) {
+      if (isParentIncluded(sorted, us._master)) {
         // Push element to sorted array
-        sorted.push(us[name]);
+        sorted.push(us._name);
         // Remove element from unsorted
-        unsorted = unsorted.filter(u => u[name] !== us[name]);
+        unsorted = unsorted.filter(u => u._name !== us._name);
       }
     }
   }
@@ -37,4 +79,5 @@ const execute = (tables, master = 'master', name = 'name') => {
 module.exports = {
   isParentIncluded,
   execute,
+  get,
 };
