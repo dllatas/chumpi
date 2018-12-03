fa778390607358e9f2764f169b9bfbc9fc87ec21
Daniel Llatas Spiers <danllatas@gmail.com>
2018-12-03 14:10:46 +0100

    Update test for io module

--- test/io.js
+++ test/io.js
@@ -7,25 +7,27 @@ describe('io functions test suite', () => {
 
   it('capture should return an object with at least the same number of mandatory props as optionsKey', () => {
     const captured = io.capture(input);
-    // assert.equal(Object.keys(captured).length, io.optionsKey.length);
-    assert.equal(Object.keys(captured).length, 2);
+    const expected = Object.keys(io.schema).filter(s => io.schema[s].mandatory).length;
+    assert.strictEqual(Object.keys(captured).length, expected);
   });
 
   it('capture should return an object with at least the same mandatory props as optionsKey', () => {
     const captured = io.capture(input);
-    // assert.deepEqual(Object.keys(captured), io.optionsKey);
-    assert.deepEqual(Object.keys(captured), ['dir', 'format']);
+    const expected = Object.keys(io.schema).filter(s => io.schema[s].mandatory);
+    assert.deepEqual(Object.keys(captured), expected);
   });
 
   it('capture should not split input option if it does not contain symbol =', () => {
     const wrongInput = ['node', 'teseo', 'dir=./test', 'format:yaml'];
     const captured = io.capture(wrongInput);
-    assert.notStrictEqual(Object.keys(captured), io.optionsKey);
+    const expected = { dir: './test' };
+    assert.deepEqual(captured, expected);
   });
 
   it('capture should not add to output a prop not defined in options', () => {
-    const wrongInput = ['node', 'teseo', 'dest=./test', 'format=yaml'];
+    const wrongInput = ['node', 'teseo', 'godzilla=./test', 'format=yaml'];
     const captured = io.capture(wrongInput);
-    assert.notStrictEqual(Object.keys(captured), io.optionsKey);
+    const expected = { format: 'yaml' };
+    assert.notStrictEqual(captured, expected);
   });
 });
