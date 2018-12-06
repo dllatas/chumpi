0fc30e9c433cdf20984e3c4f2118577f30ad0312
Daniel Llatas Spiers <danllatas@gmail.com>
2018-12-06 22:19:52 +0100

    Refactor CLI code to src

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const validation = require('./src/validation');
-const file = require('./src/file');
-const conversion = require('./src/conversion');
-const sort = require('./src/sort');
-const io = require('./src/io');
-
-async function teseo(options) {
-  validation.enforce(options, io.schema);
-  const files = await file.read(options.dir);
-  const tables = await conversion.execute('parse', options.format, files);
-  const sorted = sort.execute(tables, options.master, options.name);
-  return sorted;
-}
+const { teseo } = require('./src');
 
 module.exports = teseo;
