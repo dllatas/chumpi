92ab20afc7aed32e7b09f5d356019f087b2c455e
Daniel Llatas Spiers <danllatas@gmail.com>
2018-12-16 21:24:44 +0100

    Add intermediate function to centralize console calls

--- cli.js
+++ cli.js
@@ -1,4 +1,17 @@
 #!/usr/bin/env node
 const { cli } = require('./src');
 
-cli(process.argv);
+async function _cli(input) {
+  try {
+    const result = await cli(input);
+    if (result.console) {
+      console.log(result.message);
+      return;
+    }
+    console.info(`Teseo escaped the Labyrinth! Open the file at ${result}`);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+_cli(process.argv);
