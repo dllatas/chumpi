c74e86b4d74c563969e29a3b537018e6cbd43c07
Daniel Llatas Spiers <danllatas@gmail.com>
2019-03-29 09:19:57 +0100

    Expose logging module with Chumpi

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const conversion = require('./conversion');
 const file = require('./file');
 const io = require('./io');
 const validation = require('./validation');
+const logging = require('./logging');
 
 module.exports = {
   conversion,
   file,
   io,
   validation,
+  logging,
 };
